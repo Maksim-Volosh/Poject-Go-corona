c27d2e1bb89d2ce189090dac6965349d540f1e79
Maksim <maksgoza9@gmail.com>
2024-01-14 15:40:41 +0200

    Fix buttons

--- pdj/static/js/js.js
+++ pdj/static/js/js.js
@@ -4,6 +4,16 @@ document.querySelector('.download-button').addEventListener('click', function() 
     modal.classList.add('download-modal-open'); // Добавляем класс modal-open к элементу
 });
 
+document.querySelector('.download-button2').addEventListener('click', function() {
+    var modal = document.getElementById('download-modal');
+    modal.classList.add('download-modal-open'); // Добавляем класс modal-open к элементу
+});
+
+document.querySelector('.download-button3').addEventListener('click', function() {
+    var modal = document.getElementById('download-modal');
+    modal.classList.add('download-modal-open'); // Добавляем класс modal-open к элементу
+});
+
 // При нажатии на кнопку закрыть
 document.querySelector('.close-download-button').addEventListener('click', function() {
     var modal = document.getElementById('download-modal');
